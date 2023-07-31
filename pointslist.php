b398b3bdaf5f16e77272149f88c3b672cdda91e2
Timmeih <140506655+Timmeih@users.noreply.github.com>
2023-07-31 17:46:28 +1000

    Css

--- pointslist.php
+++ pointslist.php
@@ -1,32 +1,25 @@
 <!DOCTYPE html>
 <html>
-
-
 <header>
     <title>GNAR</title>
     <?php require('templates/header.php'); ?>
 </header>
-
-
 <body>
-<div class ="shell">
-    <h1>List of points</h1>
-    <nav>
-        <ur>
-            <li><a href="#Runs">Runs</a></li>
-            <li><a href="#Tricks">Tricks</a></li>
-            <li><a href="#ECPS">ECPS</a></li>
-        </ur>
-    </nav>
-    <div class="shell">
-    <a href="../">Back</a>
+    <div class ="shell">
+        <h1>List of points</h1>
+        <nav>
+            <ur>
+                <li><a href="#Runs">Runs</a></li>
+                <li><a href="#Tricks">Tricks</a></li>
+                <li><a href="#ECPS">ECPS</a></li>
+            </ur>
+        </nav>
+        <a href="../">Back</a>
     </div>
 </body>
+<footer>
 
-</div>
-    <footer>
-
-    </footer>
+</footer>
 
 
 </html>
--- templates/header.php
+++ templates/header.php
@@ -3,5 +3,10 @@
 <link rel="icon" type="image/x-icon" href="images/russ_icon.ico" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+<!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> -->
+<link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+
+
 <link rel="stylesheet" href="styles/main.css" />
 <script src="js/main.js" async></script>
